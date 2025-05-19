4db764704c936212dd25f0f606fd248271a1cc7f
panda-pritam <mondalrumki02@gmail.com>
2025-05-19 23:36:58 +0530

    dada

--- gis_backend/gis_backend/settings.py
+++ gis_backend/gis_backend/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React dev server
-    # "https://yourfrontenddomain.com",
+    "http://178.236.185.244:8008", "http://178.236.185.244:3000",
+    
 ]
 
